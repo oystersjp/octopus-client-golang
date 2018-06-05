f2beb636481fd7d358d019559d9c8fd3c23d91fa
Hokuto Shinoda <pinkumohikan@gmail.com>
2018-06-05 23:32:00 +0900

    feat: PresharedKey認証に対応する

--- client.go
+++ client.go
@@ -6,28 +6,55 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 const ApiBaseUrl = "https://strong-octopus.com"
 
 type Client struct {
+	presharedKey string
 	httpClient http.Client
 }
 
-func NewClient() Client {
+func NewClient(presharedKey string) Client {
 	return Client{
+		presharedKey: presharedKey,
 		httpClient: http.Client{},
 	}
 }
 
 func (c *Client) SearchByKeyword(keyword string, page int) ([]Article, error) {
 	var path = fmt.Sprintf("/searchResponse/search?keyword=%s&page=%d", keyword, page)
-	var endpoint = fmt.Sprintf("%s%s", ApiBaseUrl, path;
 
-	req, err := http.NewRequest("GET", endpoint, nil)
+	params := url.Values{}
+	params.Add("keyword", keyword)
+	params.Add("page", strconv.Itoa(page))
+	responseBody, err := c.request("GET", path, params)
 	if err != nil {
 		return nil, err
 	}
+
+	var searchResponse struct {
+		Articles []Article `json:"articles"`
+	}
+
+	if err := json.Unmarshal(responseBody, &searchResponse); err != nil {
+		return nil, err
+	}
+
+	return searchResponse.Articles, nil
+}
+
+func (c *Client) request(method string, path string, params url.Values) ([]byte, error) {
+	var endpoint = fmt.Sprintf("%s%s?%s", ApiBaseUrl, path, params.Encode())
+
+	req, err := http.NewRequest(method, endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.presharedKey))
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
@@ -51,13 +78,5 @@ func (c *Client) SearchByKeyword(keyword string, page int) ([]Article, error) {
 		return nil, errors.New(errorResponse.Message)
 	}
 
-	var searchResponse struct {
-		Articles []Article `json:"searchResponse"`
-	}
-
-	if err := json.Unmarshal(body, &searchResponse); err != nil {
-		return nil, err
-	}
-
-	return searchResponse.Articles, nil
+	return body, nil
 }
