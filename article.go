36d225eded2dea8c218d3015daf9098ae871df45
Hokuto Shinoda <pinkumohikan@gmail.com>
2018-06-13 00:22:19 +0900

    feat: PublisherName,PublisherUrlのカラム追加に対応

--- article.go
+++ article.go
@@ -1,9 +1,11 @@
 package octopus
 
 type Article struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Summary     string `json:"summary"`
-	Url         string `json:"url"`
-	PublishedAt string `json:"publishedAt"`
+	Id            int    `json:"id"`
+	Title         string `json:"title"`
+	Summary       string `json:"summary"`
+	Url           string `json:"url"`
+	PublishedAt   string `json:"publishedAt"`
+	PublisherName string `json:"publisherName"`
+	PublisherUrl  string `json:"publisherUrl"`
 }
